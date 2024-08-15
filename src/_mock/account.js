5231c61089712ce57f066e8d74f33c966e2bbe2f
Muhammad ILYAS <ilyas02828@gmail.com>
2024-08-15 16:09:19 +0500

    new dash uploadin

---
+++ src/_mock/account.js
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'Ilyas02828',
+  email: 'Ilyas02828@gmail.com',
+  photoURL: '/assets/images/avatars/avatar_25.jpg',
+};
